f7f010451237b061e0f9706e1781c8f646c0a78e
Levy Medina II <levymedina3@gmail.com>
2018-02-28 13:32:15 +0800

    added nodemon

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var express = require('express')
 var app = express()
 require('dotenv').config()
 var bodyParser = require('body-parser')
-var port = process.env.port || 5000
 var gather = require('./controllers/gather-resource')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/gather', gather)
+
+var port = process.env.port || 5000
 
 app.listen(port, () => {
   console.log('App running at port:', port)
