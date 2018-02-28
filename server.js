7050f7b1450094aa81e76834f4278180d0dd877c
Levy Medina II <levymedina3@gmail.com>
2018-02-28 13:20:46 +0800

    added body parser

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express')
 var app = express()
-var port = 5000
-
+require('dotenv').config()
+var bodyParser = require('body-parser')
+var port = process.env.port || 5000
 var gather = require('./controllers/gather-resource')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 app.use('/gather', gather)
 
-app.listen(port)
+app.listen(port, () => {
+  console.log('App running at port:', port)
+})
